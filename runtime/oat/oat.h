9571f246e84310b955c2c71295311a51dd97d914
Victor Chang <vichang@google.com>
2025-06-23 12:15:12 +0100

    Implement Thread.setCurrentThread()
    No behavior change of Thread.currentThread() is expected in this CL,
    because no one actually calls Thread.setCurrentThread().
    
    Flag: EXEMPT refactor
    Bug: 346542404
    Test: testrunnner.py -b --host
    Change-Id: Ie3ec6fc5123de959e23af08b74d104256bb5b6e3

--- runtime/oat/oat.h
+++ runtime/oat/oat.h
@@ -47,8 +47,8 @@ std::ostream& operator<<(std::ostream& stream, StubType stub_type);
 class EXPORT PACKED(4) OatHeader {
  public:
   static constexpr std::array<uint8_t, 4> kOatMagic { { 'o', 'a', 't', '\n' } };
-  // Last oat version changed reason: class_flags changes required for using MOVE ioctl.
-  static constexpr std::array<uint8_t, 4> kOatVersion{{'2', '6', '2', '\0'}};
+  // Last oat version changed reason: Add the current_peer field to Thread.
+  static constexpr std::array<uint8_t, 4> kOatVersion{{'2', '6', '3', '\0'}};
 
   static constexpr const char* kDex2OatCmdLineKey = "dex2oat-cmdline";
   static constexpr const char* kDebuggableKey = "debuggable";
